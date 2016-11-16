e84bb66e6353e1506f4c28145b98e50710ff3bf7
Thomas Eckhardt <mail@thomaseckhardt.com>
2016-11-16 17:25:17 +0100

    Import `audi_type` repository

---
+++ README.md
@@ -0,0 +1,31 @@
+# Audi Type
+
+[![Deployment status from DeployBot](https://strichpunktdesign.deploybot.com/badge/23779030048235/65315.svg)](http://deploybot.com)
+
+## Getting Started
+
+### Embed into your projects
+
+Just link `audi-type.min.css` from your HTML files:
+
+```html
+<link rel="stylesheet" href="http://dev.strichpunkt-design.de/audi/type/audi-type.min.css">
+```
+
+A [preview](http://dev.strichpunkt-design.de/audi/type/) of provided font-faces is also available.
+
+### Build
+
+To get started, first install the necessary dependencies, from the root of the project:
+
+```
+npm i
+```
+
+> Audi Type requires NodeJS 4.5 and Gulp 4.0
+
+Next, run the following one-liner to compile all files to `dist`
+
+```
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+// TODO Add copyright and license information
+
+'use strict';
+
+const _ = require('gulp-load-plugins')();
+const del = require('del');
+const gulp = require('gulp');
+const pkg = require('./package.json');
+
+
+// ----------------------------------------------------------------------------
+// CONFIG
+// ----------------------------------------------------------------------------
+
+const libraryName = 'audi-type';
+
+const banner = `/**
+ * ${pkg.name} - ${pkg.description}
+ * @version v${pkg.version}
+ * @license ${pkg.license}
+ * @copyright ${new Date().getFullYear()} ${pkg.author}
+ * @link ${pkg.homepage}
+ */\n`;
+
+
+// ----------------------------------------------------------------------------
+// Build
+// ----------------------------------------------------------------------------
+
+gulp.task('build:css', function() {
+  return gulp.src('src/scss/*.scss')
+    .pipe(_.plumber())
+    .pipe(_.sass({
+      precision: 10
+    }))
+    .pipe(_.header(banner))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(_.if('*.css', _.csso()))
+    .pipe(_.header(banner))
+    .pipe(_.rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(_.size({
+      title: _.util.colors.underline('CSS size:') + '\n',
+      showFiles: true,
+      showTotal: false,
+      gzip: true
+  }));
+});
+
+gulp.task('build:copy-fonts', function() {
+  return gulp.src('src/fonts/*.+(eot|svg|ttf|woff|woff2)')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('build:copy-files', function() {
+  return gulp.src(['src/*.html', 'src/.htaccess'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+  return del(['dist/css', 'dist/fonts']);
+});
+
+
+gulp.task('build', gulp.series('clean', 'build:css', 'build:copy-fonts', 'build:copy-files'));
+
+
+// ----------------------------------------------------------------------------
+// DEFAULT
+// ----------------------------------------------------------------------------
+
+gulp.task('default', gulp.series('build'));
